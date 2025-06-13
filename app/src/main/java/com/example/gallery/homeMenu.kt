06c6e8bf376a8d1e6ef6ca1c96c86195823c376b
Raveen <raveenJayasanka41@gmail.com>
2025-06-13 19:35:49 +0530

    minor

--- app/src/main/java/com/example/gallery/homeMenu.kt
+++ app/src/main/java/com/example/gallery/homeMenu.kt
@@ -1,19 +1,34 @@
 package com.example.gallery
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import com.example.gallery.databinding.ActivityHomeMenuBinding
+import com.example.gallery.databinding.ActivityMainBinding
 
 class homeMenu : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityHomeMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_menu)
+        binding = ActivityHomeMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(homePart())
+        binding.bottomViewPart.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.homeBottom -> replaceFragment(homePart())
+                R.id.addbottom -> replaceFragment(addInventoryPart())
+                R.id.settingsbottom -> replaceFragment(settingsPart())
+                else -> {
 
+                }
+            }
+            true
+        }
     }
     private fun replaceFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
